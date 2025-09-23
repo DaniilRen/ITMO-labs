db3c8388f0ea03aa51399ee94f1874958dd7567c
Daniil B. <fffn12jk@gmail.com>
2025-09-23 19:42:44 +0300

    Delete convert.py

--- convert.py
+++
@@ -1,6 +0,0 @@
-with open('input.txt') as src:
-    rows = [' & '.join(r.split()) + r' \\' for r in src.readlines()]
-    with open('output.txt', 'w') as out:
-        print(rows)
-        for r in rows:
-            out.write(r + "\n")
