346b2ec90cca06d20ec6fbf1a5399ec71c22590f
Daniil B. <fffn12jk@gmail.com>
2025-11-03 19:31:58 +0300

    feat(java/Lab2): finish Lab2

--- Informatics/Lab3/Informatics_Lab3_Task3.py
+++ Informatics/Lab3/Informatics_Lab3_Task3.py
@@ -1,6 +1,6 @@
 # Author = Bykh Daniil Maksimovich
 # Group = P3109
-# Date = 20.10.2025
+# Date = 03.11.2025
 
 
 from re import compile, match, fullmatch
@@ -11,8 +11,14 @@ class Task3:
 		self.tests = tests
 
 	def check(self, text: str) -> bool:
-		base_pattern = r'(\*|(\d{1,2})(-\d{1,2})?|(\*(\/)?\d{1,2}))'
-		pattern = compile(rf"^{base_pattern}\s{base_pattern}\s{base_pattern}\s{base_pattern}\s{base_pattern}$")
+		# Шаблон для числа ли диапазона чисел: 59, 12-33
+		number_pattern = r'(\d{1,2})(-\d{1,2})?'
+		# Шаблон для полей с повторяющиймся значением: *\5
+		cycle_pattern = r'(\*(\/)?\d{1,2})'
+		# Шаблон для 1 поля Cron-выражения: 59, 12-33, *\5
+		base_pattern = rf'(\*|{number_pattern}|{cycle_pattern})'
+		# Общий шаблон для всех 5 полей 
+		pattern = rf"{base_pattern}\s{base_pattern}\s{base_pattern}\s{base_pattern}\s{base_pattern}"
 		return bool(fullmatch(pattern, text))
 
 	def test(self) -> None:
@@ -31,24 +37,24 @@ class Task3:
 
 if __name__ == "__main__":
 	tests = {
-		# Valid cron expressions - correct format of fields
+		# Группа тестов 1: все поля верные
 		"30 14 * * *": True,
 		"*/5 * * * *": True,
 		"0 0 1 1 *": True,
 		"1-5 0 * * *": True,
 		"0-59 0-23 1-31 1-12 0-6": True,
 
-		# Invalid - wrong number of fields
+		# Группа тестов 2: неверное количество полей 
 		"30 14 * *": False,
 		"*/5 * *": False,
 		"0 0 1 1 * *": False,
 
-		# Invalid - illegal characters in fields
+		# Группа тестов 3: некорректные символы
 		"30 14 * * #": False,
 		"*/5 * * ! *": False,
 		"0 0 abc 1 *": False,
 
-		# Invalid - missing spaces
+		# Группа тестов 4: отсутствие пробелов между полями
 		"30 14** * *": False,
 		"*/5* * * *": False,
 	}
--- Java/Lab2/Test.java
+++ Java/Lab2/Test.java
@@ -1,11 +1,61 @@
 class Test {
     Test returnValue() {
-        return new Test();
+        return this;
     }
 }
 
 class TestChild extends Test {
+    @Override
     TestChild returnValue() {
         return new TestChild();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import java.util.List;
+
+// public static void foo(List<? extends Test> list) {}
+
+// List<TestChild> list1 = new ArrayList<>();
+// List<Test> list2 = new ArrayList<>();
+
+// foo(list1) // OK
+// foo(list2) // OK
+
+
+
+// public static void foo(List<? super TestChild> list) {}
+
+// List<TestChild> list1 = new ArrayList<>();
+// List<Test> list2 = new ArrayList<>();
+
+// foo(list1) // OK
+// foo(list2) // OK
+
+
+// public static void foo(List<TestChild> list) {}
+
+// List<TestChild> list1 = new ArrayList<>();
+// List<Test> list2 = new ArrayList<>();
+
+// foo(list1) // OK
+// foo(list2) // Incompatile types
