e383785f0d0b8b73321977ab4f37aac0b208724f
Daniil B. <fffn12jk@gmail.com>
2026-04-01 14:21:32 +0300

    fix: change ODZ

--- OPD/labs/lab4/check.py
+++ OPD/labs/lab4/check.py
@@ -2,12 +2,11 @@ from functools import lru_cache
 
 @lru_cache
 def F(x):
-    if x in range(-2726, 0):
-        return -2726
+    if -2726 <= x < 0: return -2726
     return 5*x + 77
 
-def R(x, y, z):
+def R(y, x, z):
     return F(y) - F(x-1) + F(z) - 1
 
-print(R(-6568, -6569, -6568))
-print(R(-2725, 6538, -561))
+print(R(-2726, 5448, -2726))
+print(R(2988, -2725, 2989))
